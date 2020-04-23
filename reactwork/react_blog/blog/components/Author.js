9e471a3349183f83991f3390e34a1ba0f02e64c9
fionalee <981815087@qq.com>
2020-04-23 23:08:42 +0800

    blogfirst

---
+++ reactwork/react_blog/blog/components/Author.js
@@ -0,0 +1,17 @@
+import { Avatar, Divider} from 'antd'
+import '../static/style/components/author.css'
+
+const Author = () => {
+    return (
+        <div className="author-div comm-box">
+            <div> <Avatar size={100} src="../static/img/avater.jpg" /></div>
+            <div className="author-introduction">
+                专注于WEB和移动前端开发。
+                <Divider>社交账号</Divider>
+                <Avatar size={28} icon="github" className="account" />
+                <Avatar size={28} icon="qq" className="account" />
+                <Avatar size={28} icon="wechat" className="account" />
+            </div>
+        </div>)
+}
+export default Author
